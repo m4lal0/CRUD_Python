ba00f426501b3cbe27a1d593e3e03e9c433b10e7
M4lal0 <malalocerrillo@gmail.com>
2020-07-09 14:03:15 -0500

    Creando esqueleto de main.py y cargando archivo CSV

---
+++ main.py
@@ -0,0 +1,48 @@
+# By @M4lal0
+
+import sys,csv,os
+
+CLIENT_TABLE = '.clients.csv'
+CLIENT_SCHEMA = ['name', 'company', 'email', 'position']
+clients = []
+
+
+def _initialize_clients_from_storage():
+    with open(CLIENT_TABLE, mode='r') as f:
+        reader = csv.DictReader(f, fieldnames=CLIENT_SCHEMA)
+
+        for row in reader:
+            clients.append(row)
+
+
+def display_banner():
+    print("What would you like to do today?")
+    print("[C]reate client")
+    print("[R]ead client\´s")
+    print("[U]pdate client")
+    print("[D]elete client")
+    print("[S]earch client")
+    print("[E]xit")
+
+
+if __name__ == '__main__':
+    _initialize_clients_from_storage()
+    display_banner()
+
+    command = input("Option: ")
+    command = command.upper()
+
+    if command == "C":
+        pass
+    elif command == "R":
+        pass
+    elif command == "U":
+        pass
+    elif command == "D":
+        pass
+    elif command == "S":
+        pass
+    elif command == "E":
+        sys.exit()
+    else:
+        print("Invalid command")
