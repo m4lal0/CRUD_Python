c227af7ed1ad8959446123cdd134e46ca1b73ba0
M4lal0 <malalocerrillo@gmail.com>
2020-07-11 18:27:11 -0500

    Agregando las operaciones CRUD

--- main.py
+++ main.py
@@ -44,7 +44,7 @@ def create_client(client):
     if client not in clients:
         clients.append(client)
     else:
-        print("Client already is in the client´s list")
+        print(bcolors.FAIL + "Client already is in the client´s list" + bcolors.ENDC)
 
 
 def update_client(client_id, updated_client):
@@ -53,7 +53,7 @@ def update_client(client_id, updated_client):
     if len(clients) - 1 >= client_id:
         clients[client_id] = updated_client
     else:
-        print("Client is not in client´s list")
+        print(bcolors.FAIL + "Client is not in client´s list" + bcolors.ENDC)
 
 
 def delete_client(client_id):
@@ -110,15 +110,13 @@ def display_banner():
     banner +="| ▓▓__/  \ ▓▓  | ▓▓ ▓▓__/ ▓▓ ▓▓__/ ▓▓\n"
     banner +=" \▓▓    ▓▓ ▓▓  | ▓▓\▓▓    ▓▓ ▓▓    ▓▓\n"
     banner +="  \▓▓▓▓▓▓ \▓▓   \▓▓ \▓▓▓▓▓▓ \▓▓▓▓▓▓▓ \n"
-    banner +="-" * 39 + "\n"
     banner +="--[ Create | Read | Update | Delete ]--\n"
     banner +="--[             v20.01              ]--\n"
-    banner +="-" * 39
-    return print(bcolors.OKBLUE + banner + bcolors.ENDC)
+    return print(bcolors.HEADER + banner + bcolors.ENDC)
 
 
 def display_menu():
-    print(bcolors.OKGREEN + "What would you like to do today?" + bcolors.ENDC)
+    print("What would you like to do today?")
     print("[C]reate client")
     print("[R]ead client´s")
     print("[U]pdate client")
@@ -132,7 +130,7 @@ if __name__ == '__main__':
     display_banner()
     display_menu()
 
-    command = input("Option: ")
+    command = input(bcolors.BOLD + "Option: " + bcolors.ENDC)
     command = command.upper()
 
     if command == "C":
@@ -143,7 +141,7 @@ if __name__ == '__main__':
     elif command == "U":
         client_id = int(_get_client_field('id'))
         updated_client = _get_client_from_user()
-        update_client(client_id, update_client)
+        update_client(client_id, updated_client)
     elif command == "D":
         client_id = int(_get_client_field('id'))
         delete_client(client_id)
@@ -151,12 +149,12 @@ if __name__ == '__main__':
         client_name = _get_client_field('name')
         found = search_client(client_name)
         if found:
-            print("The client is in the client´s list")
+            print(bcolors.OKGREEN + "The client is in the client´s list" + bcolors.ENDC)
         else:
-            print("The client: {0} is not in our client´s list".format(client_name))
+            print(bcolors.FAIL+ "The client: {0} is not in our client´s list".format(client_name) + bcolors.ENDC)
     elif command == "E":
         sys.exit()
     else:
-        print("Invalid command")
+        print(bcolors.FAIL + "Invalid command" + bcolors.ENDC)
     
     _save_clients_to_storage()
